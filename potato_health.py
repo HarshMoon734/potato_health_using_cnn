29084af37b0448b1bc29387cb4796e785d63cf07
HarshMoon734 <harshanna30@gmail.com>
2024-06-30 23:32:02 +0530

    Update potato_health.py

--- potato_health.py
+++ potato_health.py
@@ -1,105 +1,66 @@
-import tensorflow as tf
-from tensorflow.keras import models, layers
-import matplotlib.pyplot as plt
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import gdown
-import zipfile
 import os
 
-# Download and extract the dataset from Google Drive
-url = 'https://drive.google.com/uc?id=1DXxwhcbHhB_qyv8PZFYqwyakVal0hyeI'
-output = 'dataset.rar'
-gdown.download(url, output, quiet=False)
+import numpy as np
 
-# Extract the contents of the WinRAR file
-with zipfile.ZipFile(output, 'r') as zip_ref:
-    zip_ref.extractall('./dataset')
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-# Define image dimensions and channels
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.preprocessing import image
+
+from tensorflow.keras.models import load_model
+
 IMAGE_SIZE = 256
-CHANNELS = 3
 
-# Data generators
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=10,
-    horizontal_flip=True
+train_dir = r'C:\Users\Harsh\Downloads\potato_\PlantVillage\real\Potato___Early_blight'
+val_dir = r'C:\Users\Harsh\Downloads\potato_\PlantVillage\real\Potato___healthy'
+test_dir = r'C:\Users\Harsh\Downloads\potato_\PlantVillage\real\Potato___Late_blight'
+
+def see_image(training, index):
+    files = os.listdir(training)
+    # print(files[0])
+    img = mpimg.imread(f"{training}\{files[index]}")
+    plt.imshow(img)
+    plt.show()
+
+files = os.listdir(test_dir)
+
+dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    r"C:\Users\Harsh\Downloads\potato_\PlantVillage\real",
+    shuffle = True,
+    image_size = (256,256),
+    batch_size = 32
 )
 
-train_generator = train_datagen.flow_from_directory(
-    'dataset/train',
-    target_size=(IMAGE_SIZE, IMAGE_SIZE),
-    batch_size=32,
-    class_mode="sparse",
-    save_to_dir="./potato-disease-classification/training/AugmentedImages"
-)
-
-validation_datagen = ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=10,
-    horizontal_flip=True
-)
-
-validation_generator = validation_datagen.flow_from_directory(
-    'dataset/val',
-    target_size=(IMAGE_SIZE, IMAGE_SIZE),
-    batch_size=32,
-    class_mode="sparse"
-)
-
-test_datagen = ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=10,
-    horizontal_flip=True
-)
-
-test_generator = test_datagen.flow_from_directory(
-    'dataset/test',
-    target_size=(IMAGE_SIZE, IMAGE_SIZE),
-    batch_size=32,
-    class_mode="sparse"
-)
-
-# Define model architecture
-input_shape = (IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
-n_classes = 3
+train_dataset = dataset.take(int(len(dataset)*80/100))
+test_dataset = dataset.take(int(len(dataset)*10/100))
+validation_dataset = dataset.take(int(len(dataset)*10/100))
 
 model = models.Sequential([
-    layers.InputLayer(input_shape=input_shape),
-    layers.Conv2D(32, kernel_size=(3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3)),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(64, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Conv2D(128, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Conv2D(128, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
     layers.Flatten(),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(n_classes, activation='softmax'),
+    layers.Dense(512, activation='relu'),
+    layers.Dense(3, activation='softmax')
 ])
 
-# Compile and train the model
-model.compile(
-    optimizer='adam',
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-    metrics=['accuracy']
-)
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
-history = model.fit(
-    train_generator,
-    steps_per_epoch=47,  # Adjust this based on your dataset size and batch size
-    batch_size=32,
-    validation_data=validation_generator,
-    validation_steps=6,  # Adjust based on your validation dataset size and batch size
-    verbose=1,
-    epochs=20,
-)
+history = model.fit(train_dataset,
+                    validation_data=validation_dataset,
+                    epochs=10)
 
-# Evaluate the model on the test set
-scores = model.evaluate(test_generator)
-print(scores[1])
+test_loss, test_acc = model.evaluate(test_dataset, verbose=2)
+
+# print(model.evaluate(test_dataset)) # if you want to evaluate the model on the bases of test_dataset
+
