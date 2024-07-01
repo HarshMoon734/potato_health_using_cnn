21ba1fc80ac69829e5ce598ccfad32bd0f58af6b
HarshMoon734 <harshanna30@gmail.com>
2024-07-01 15:57:31 +0530

    Update potato_health.py

--- potato_health.py
+++ potato_health.py
@@ -14,32 +14,30 @@ from tensorflow.keras.models import load_model
 
 IMAGE_SIZE = 256
 
-train_dir = r'C:\Users\Harsh\Downloads\potato_\PlantVillage\real\Potato___Early_blight'
-val_dir = r'C:\Users\Harsh\Downloads\potato_\PlantVillage\real\Potato___healthy'
-test_dir = r'C:\Users\Harsh\Downloads\potato_\PlantVillage\real\Potato___Late_blight'
+_dirname = r'C:\Users\Harsh\Downloads\potato_\PlantVillage\real'
 
 def see_image(training, index):
     files = os.listdir(training)
-    # print(files[0])
     img = mpimg.imread(f"{training}\{files[index]}")
     plt.imshow(img)
     plt.show()
 
-files = os.listdir(test_dir)
-
-dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    r"C:\Users\Harsh\Downloads\potato_\PlantVillage\real",
+train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    _dirname,
     shuffle = True,
     image_size = (256,256),
     batch_size = 32
 )
 
-train_dataset = dataset.take(int(len(dataset)*80/100))
-test_dataset = dataset.take(int(len(dataset)*10/100))
-validation_dataset = dataset.take(int(len(dataset)*10/100))
+class_names_length = train_dataset.class_names
+
+train_dataset = train_dataset.take(int(len(train_dataset)*80/100))
+validation_dataset = train_dataset.take(int(len(train_dataset)*10/100))
+test_dataset = train_dataset.take(int(len(train_dataset)*10/100))
 
 model = models.Sequential([
-    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3)),
+    layers.Rescaling(1./255),
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(64, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
@@ -49,17 +47,16 @@ model = models.Sequential([
     layers.MaxPooling2D((2, 2)),
     layers.Flatten(),
     layers.Dense(512, activation='relu'),
-    layers.Dense(3, activation='softmax')
+    layers.Dense(len(class_names_length), activation='softmax')
 ])
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-history = model.fit(train_dataset,
-                    validation_data=validation_dataset,
-                    epochs=10)
+history = model.fit(train_dataset, epochs=50, validation_data=validation_dataset, verbose = 1)
 
+print(train_dataset.class_names)
 test_loss, test_acc = model.evaluate(test_dataset, verbose=2)
 
 # print(model.evaluate(test_dataset)) # if you want to evaluate the model on the bases of test_dataset
